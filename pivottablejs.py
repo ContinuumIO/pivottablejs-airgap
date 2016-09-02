dab264228973e978dcd7e600db9b5ca1f9df861e
Ilan Schnell <ilanschnell@gmail.com>
2016-09-01 23:09:30 -0500

    update links to point to static files

--- pivottablejs.py
+++ pivottablejs.py
@@ -8,20 +8,20 @@ template = """
 <html>
     <head>
         <title>PivotTable.js</title>
-        
-        <!-- external libs from cdnjs -->
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css">
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
 
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/pivot.min.css">
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/pivot.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/d3_renderers.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/c3_renderers.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/1.6.3/export_renderers.min.js"></script>
+        <!-- external libs from cdnjs -->
+        <link rel="stylesheet" type="text/css" href="%(static)s/ajax/c3.min.css">
+        <script type="text/javascript" src="%(static)s/ajax/jquery.min.js"></script>
+        <script type="text/javascript" src="%(static)s/ajax/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="%(static)s/ajax/libs/d3/3.5.5/d3.min.js"></script>
+        <script type="text/javascript" src="%(static)s/ajax/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
+        <script type="text/javascript" src="%(static)s/ajax/c3.min.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="%(static)s/pivottable/pivot.min.css">
+        <script type="text/javascript" src="%(static)s/pivottable/pivot.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivottable/d3_renderers.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivottable/c3_renderers.min.js"></script>
+        <script type="text/javascript" src="%(static)s/pivottable/export_renderers.min.js"></script>
 
         <style>
             body {font-family: Verdana;}
@@ -47,13 +47,13 @@ template = """
                 if(window.location != window.parent.location)
                     $("<a>", {target:"_blank", href:""})
                         .text("[pop out]").prependTo($("body"));
-                    
-                $("#output").pivotUI( 
-                    $.csv.toArrays($("#output").text()), 
-                    { 
+
+                $("#output").pivotUI(
+                    $.csv.toArrays($("#output").text()),
+                    {
                         renderers: $.extend(
-                            $.pivotUtilities.renderers, 
-                            $.pivotUtilities.c3_renderers, 
+                            $.pivotUtilities.renderers,
+                            $.pivotUtilities.c3_renderers,
                             $.pivotUtilities.d3_renderers,
                             $.pivotUtilities.export_renderers
                             ),
@@ -62,15 +62,17 @@ template = """
                 ).show();
              });
         </script>
-        <div id="output" style="display: none;">%s</div>
+        <div id="output" style="display: none;">%(div)s</div>
     </body>
 </html>
 """
 
+import sys
 from IPython.display import IFrame
 
 def pivot_ui(df, outfile_path = "pivottablejs.html", width="100%", height="500"):
     with open(outfile_path, 'w') as outfile:
-        outfile.write(template % df.to_csv())
+        outfile.write(template % {'div': df.to_csv(),
+                                  'static': 'file://%s/static' % sys.prefix})
     return IFrame(src=outfile_path, width=width, height=height)
-        
+
